d0efffa178315c202d8ad75bfbb28020aced4c30
anton202 <a.kluge202@gmail.com>
2019-09-11 09:11:05 +0300

    testing https

--- server/app.js
+++ server/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const fs = require('fs');
+const https = require('https');
 const app = express();
 const path = require('path');
 
-
 app.use(express.static(path.join('../weatherApp/dist/weatherApp')))
 
-app.listen(8084,()=>console.log(8084))
+https.createServer({
+    key: fs.readFileSync('/root/encryption/server.key'),
+    cert: fs.readFileSync('/root/encryption/server.cert')
+  }, app)
+  .listen(8084)
