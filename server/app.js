0c68c4fd5a28db7406882d85437469e08885f349
anton202 <a.kluge202@gmail.com>
2019-09-11 09:24:49 +0300

    back to http

--- server/app.js
+++ server/app.js
@@ -1,13 +1,8 @@
 const express = require('express');
-const fs = require('fs');
-const https = require('https');
 const app = express();
 const path = require('path');
 
+
 app.use(express.static(path.join('../weatherApp/dist/weatherApp')))
 
-https.createServer({
-    key: fs.readFileSync('/root/encryption/server.key'),
-    cert: fs.readFileSync('/root/encryption/server.cert')
-  }, app)
-  .listen(8084)
+app.listen(8084,()=>console.log(8084))
